d4f7fc926579eb1397d77a8ff218fca10d4e4626
rvalencia <renso121992@gmail.com>
2018-11-02 21:08:02 -0500

    Terminar otro avance

--- build/generated-sources/ap-source-output/com/upc/indra/be/DetSolParticipante_.java
+++ build/generated-sources/ap-source-output/com/upc/indra/be/DetSolParticipante_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-28T11:20:00")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-28T19:42:26")
 @StaticMetamodel(DetSolParticipante.class)
 public class DetSolParticipante_ { 
 
--- build/generated-sources/ap-source-output/com/upc/indra/be/DetalleSolicitud_.java
+++ build/generated-sources/ap-source-output/com/upc/indra/be/DetalleSolicitud_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-28T11:20:00")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-28T19:42:26")
 @StaticMetamodel(DetalleSolicitud.class)
 public class DetalleSolicitud_ { 
 
